f20d8bf29ce4147fae41b85e3369e9afafbdc885
James <majam@sheridanc.on.ca>
2019-04-13 16:10:56 -0400

    revamped index.js
    -> no longer using Nearby Places API
    -> switched to Text Search API
    - default parameter for input: 'restaurant'
    - can directly call API with URL for photos
    - can no longer access photos directly
    
    TODO: extract photo reference and plug into API call to generate photos

--- www/js/index.js
+++ www/js/index.js
@@ -3,13 +3,9 @@ var app = {
     // Application Constructor
     initialize: function() {
         document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
-        $(document).ready(startSearch);
     },
 
     // deviceready Event Handler
-    //
-    // Bind any cordova events here. Common events are:
-    // 'pause', 'resume', etc.
     onDeviceReady: function() {
         this.receivedEvent('deviceready');
     },
@@ -25,130 +21,65 @@ var app = {
 
         console.log('Received Event: ' + id);
     }
+
 };
 
 app.initialize();
 
-function startSearch(){
+function getLocation() {
 
-    //initializing variables
     navigator.geolocation;
-    var placesList = document.getElementById('places');
-    var error = document.createElement("p");
-    var bannerMsg = document.createElement("bannerMsg")
-
-    //Error checking for geolocation
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
-
+        navigator.geolocation.getCurrentPosition(initSearch, onError(), { timeout: 30000, enableHighAccuracy: true });
+    } else {
+        console.log('geolocation unsuccessful')
     }
-    else{
-        error.textContent = "Geolocation is not supported";
-        placesList.appendChild(error);
+}
+
+function onError(){
+    var page= document.getElementById("page");
+    page.textContent+="Geolocation Error";
+}
+
+function initSearch(position){
+
+    var lat= position.coords.latitude;
+    var lng= position.coords.longitude;
+    var location = new google.maps.LatLng(lat,lng);
+
+
+    var request = {
+        location:location,
+        radius: 1500,
+        query: ['restaurant'],
+
+    };
+
+    var page= document.getElementById("page");
+    var restaurantView = document.getElementById("restaurantView");
+    page.textContent+=location;
+    var service = new google.maps.places.PlacesService(restaurantView);
+    service.textSearch(request, callback);
+
+    function callback(place, status) {
+        if (status == google.maps.places.PlacesServiceStatus.OK) {
+            showRestaurants(place);
+        }
     }
+}
 
+function showRestaurants(place){
 
+    var imgRequestUrl = "https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=";
+    var API_KEY = "AIzaSyBcKQuhMx3T7GqjQFS9se0zphzm2iMR0Bk";
 
-    function showPosition() {
+    var restaurantView= document.getElementById("restaurantView");
+    var placesList = document.getElementById("places");
 
-        var restaurants;
-        initMap();
-
-        function initMap() {
-
-            // Create the map.
-            //var latlon = {lat: position.coords.latitude , lng: position.coords.longitude };
-            var latlon = {lat:-33.867, lng:151.1957362};
-            error.textContent += latlon;
-
-            // Storing current location
-            restaurants = new google.maps.Map(document.getElementById('restaurantView'), {
-                center: latlon,
-                zoom: 17
-            });
-
-
-            // Create the places service.
-            var service = new google.maps.places.PlacesService(restaurants);
-
-            // Button for loading more results
-            var getNextPage = null;
-            var moreButton = document.getElementById('more');
-            moreButton.onclick = function() {
-                moreButton.disabled = true;
-                moreButton.style.display = "none";
-                if (getNextPage) getNextPage();
-            };
-
-
-            // Perform a nearby search.
-            service.nearbySearch(
-                {location: latlon, radius: 1500, type: ['restaurant']},
-                function(results, status, pagination) {
-                    if (status !== 'OK') {
-                        console.log("results not ok");
-                        return;
-                    }
-
-                    createMarkers(results);
-                    moreButton.disabled = !pagination.hasNextPage;
-                    getNextPage = pagination.hasNextPage && function() {
-                        pagination.nextPage();
-                    };
-                });
-        }
-
-        function createMarkers(places) {
-
-            var bounds = new google.maps.LatLngBounds();
-            var placesList = document.getElementById('places');
-
-            for (var i = 0; i < 10; i++) {
-                place = places[i];
-
-                var photos = place.photos;
-                if(!photos){
-                    console.log('error');
-
-                }
-                // using local storage for data persistence
-                sessionStorage.setItem("placeName" + i, place.name);
-                sessionStorage.setItem("placeLocationLat" + i, place.geometry.location.lat() );
-                sessionStorage.setItem("placeLocationLng" + i, place.geometry.location.lng() );
-
-                // generating containers for the search results
-                var img = document.createElement("img");
-                var restaurantNamePanel = document.createElement('div');
-                var restaurantInfoPanel = document.createElement("div");
-                var restaurantPage = document.createElement('button');
-                var ImgSrc = place.photos[0].getUrl();
-
-                // setting names/pictures, classes for css
-                restaurantInfoPanel.className="infoPanel";
-                restaurantNamePanel.className="namePanel";
-                restaurantNamePanel.textContent = place.name;
-                img.src = ImgSrc;
-                restaurantPage.className="button-links";
-                restaurantPage.style.backgroundImage="url(" + ImgSrc + ")";
-                restaurantPage.value=i;
-
-                // implementing the containers
-                placesList.appendChild(restaurantInfoPanel);
-                restaurantInfoPanel.appendChild(restaurantPage);
-                restaurantInfoPanel.appendChild(restaurantNamePanel);
-
-                bounds.extend(place.geometry.location);
-            }
-            restaurants.fitBounds(bounds);
-        }
-
-        // sending the restaurant links to the map page
-        var places = document.getElementById('places');
-
-        $(places).on('click', 'button', function(){
-            sessionStorage.setItem("selectedRestaurant", this.value);
-            window.location.href = "map.html";
-        })
-
+    var img = document.createElement("img");
+    img.src=imgSrc;
+    restaurantView.append(img);
+    for (var i=0; i < 1; i++){
+        var restaurant = place[i];
     }
 }
